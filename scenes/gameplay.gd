9d7910e2a9549335c481a03f39125508b50d3533
Martin Linklater <mslinklater@gmail.com>
2022-07-17 23:06:23 +0100

    Loaded map data

--- scenes/gameplay.gd
+++ scenes/gameplay.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
+var numFlagsRemaining: int
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	# build the scene from the data files...
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
--- scripts/GameRoot.gd
+++ scripts/GameRoot.gd
@@ -1,10 +1,25 @@
 extends Node
 
 var gameplayScene: PackedScene
+var mapsEasy = {}
+var mapsMedium = {}
+var mapsHard = {}
+var mapsExtreme = {}
 
 func _ready():
 	Log.log(Log.Level.Verbose, "GameRoot _ready")
 	gameplayScene = load("res://scenes/gameplay.tscn")
+	mapsEasy = loadFile("res://json/maps_easy.tres")	
+	mapsMedium = loadFile("res://json/maps_medium.tres")	
+	mapsHard = loadFile("res://json/maps_hard.tres")	
+	mapsExtreme = loadFile("res://json/maps_extreme.tres")	
+	
+func loadFile(filename):
+	var thefile = File.new()
+	thefile.open(filename, File.READ)
+	var dict = parse_json(thefile.get_as_text())
+	thefile.close()
+	return dict
 	
 func initialise():
 	Log.log(Log.Level.Verbose, "GameRoot initialise")
